49de328805fb25d1084fadcf2805b6c6af692f6a
hyy <hyy@C02WT5SXG8WN.local>
2020-06-07 17:18:32 +0800

    init

--- misc/multistream-select/src/lib.rs
+++ misc/multistream-select/src/lib.rs
@@ -1,7 +1,7 @@
 mod protocol;
 
 use futures_util::io::{AsyncReadExt, AsyncWriteExt};
-
+use protocol::GetVarintLen;
 
 fn get_varint_len() -> u32 {
     0
@@ -14,23 +14,18 @@ fn server_test() {
         let listener = async_std::net::TcpListener::bind("127.0.0.1:5679").await.unwrap();
         let mut connec = listener.accept().await.unwrap().0;
         let mut len_buf = [0u8; 1];
-        let mut len: u32 = 0;
-        let step: u32 = 7;
-        let mut pos: u32 = 0;
-     //   let mut test_buf: Vec<u8> = vec![0x02, 0xac];
+        let mut varint_buf: Vec<u8> = Vec::new();
         loop {
             connec.read_exact(&mut len_buf).await.unwrap();
-       //     len_buf[0] =test_buf.pop().unwrap();
+
             if len_buf[0] & 0x80 == 0 {
-                len = ((len_buf[0]  as u32) << pos)  | len;
+                varint_buf.push(len_buf[0]);
                 break;
             } else {
-                len = (((len_buf[0]&0x7f) as u32) << pos)| len;
-                pos += step;
+                varint_buf.push(len_buf[0]& 0x7f);
             }
         }
-
-
+        let mut len =  GetVarintLen(varint_buf);
 
         let mut read_buf = vec![0u8; len as usize];
         connec.read_exact(&mut read_buf).await.unwrap();
@@ -42,7 +37,20 @@ fn server_test() {
             Ok(()) => (),
             Err(e) => println!("write err:{:?}", e),
         }
-        let mut read_buf = vec![0u8; 2];
+
+        let mut varint_buf: Vec<u8> = Vec::new();
+        loop {
+            connec.read_exact(&mut len_buf).await.unwrap();
+
+            if len_buf[0] & 0x80 == 0 {
+                varint_buf.push(len_buf[0]);
+                break;
+            } else {
+                varint_buf.push(len_buf[0]& 0x7f);
+            }
+        }
+        len =  GetVarintLen(varint_buf);
+        let mut read_buf = vec![0u8; len as usize];
         connec.read_exact(&mut read_buf).await.unwrap();
         println!("buf_len:{},buf:{:?}", len, read_buf);
     });
--- misc/multistream-select/src/protocol.rs
+++ misc/multistream-select/src/protocol.rs
@@ -2,19 +2,17 @@
 /// The encoded form of a multistream-select 1.0.0 header message.
 pub const MSG_MULTISTREAM_1_0: &[u8] = b"/multistream/1.0.0\n";
 
-fn get_varint_len(mut buf: Vec<u8>) -> u32 {
+pub fn GetVarintLen(mut buf: Vec<u8>) -> u32 {
     let mut len: u32 = 0;
     let step: u32 = 7;
-    let mut pos: u32 = 0;
-    let mut len_buf = [0u8; 1];
+    let mut len_item = None;
     loop {
-        len_buf[0] = buf.pop().unwrap();
-        if len_buf[0] & 0x80 == 0 {
-            len = ((len_buf[0]  as u32) << pos)  | len;
-            break;
+        len_item = buf.pop();
+        if let Some(item) = len_item{
+            len = (item as u32) | len<<step;
+
         } else {
-            len = (((len_buf[0]&0x7f) as u32) << pos)| len;
-            pos += step;
+            break;
         }
     }
     len
