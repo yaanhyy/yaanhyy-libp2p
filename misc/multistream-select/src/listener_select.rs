10cb4a05229695081e359ec10869a18a76e067d1
hyy <hyy@C02WT5SXG8WN.local>
2020-06-08 16:57:49 +0800

    debug ping server

--- misc/multistream-select/src/listener_select.rs
+++ misc/multistream-select/src/listener_select.rs
@@ -2,9 +2,12 @@ use futures_util::io::{AsyncReadExt, AsyncWriteExt};
 use futures::{AsyncRead, AsyncWrite};
 use crate::protocol;
 use super::GetVarintLen;
+use secio::config::SecioConfig;
+use secio::handshake::handshake;
+use secio::identity::Keypair;
 
 pub async fn listener_select_proto<S>(mut connec: S, protocols: Vec<String>) -> Vec<String>
- where S: AsyncRead + AsyncWrite + Send + Unpin + 'static
+ where S: AsyncRead + AsyncWrite + Send + Unpin + 'static + std::clone::Clone
 {
     let mut match_proto = Vec::new();
     let mut len_buf = [0u8; 1];
@@ -59,13 +62,28 @@ pub async fn listener_select_proto<S>(mut connec: S, protocols: Vec<String>) -> 
             let res = connec.write_all(&len_buf).await;
             let res = connec.write_all(&read_buf).await;
             match_proto.push(proto_name);
+
+            let key1 = Keypair::generate_ed25519();
+            let mut config = SecioConfig::new(key1);
+            let mut res = handshake(connec.clone(), config).await;
+            match res {
+                Ok((mut secure_conn_writer, mut secure_conn_reader) ) => {
+                    let data = secure_conn_reader.read().await.unwrap();
+                    println!("secure data:{:?}", data);
+                },
+                Err(e) => println!("res:{:?}", e),
+            }
             break;
-        } else {
+        } //else if  proto_name.eq("/secio/1.0.0\n") {
+
+        //}
+        else {
             len_buf[0] = 3;
             let res = connec.write_all(&len_buf).await;
             let res = connec.write_all(&[0x6e, 0x61, 0xa]).await;
         }
     }
+
 
     match_proto
 }
@@ -75,7 +93,13 @@ fn server_test() {
     async_std::task::block_on(async move {
         let listener = async_std::net::TcpListener::bind("127.0.0.1:5679").await.unwrap();
         let mut connec = listener.accept().await.unwrap().0;
-        let res = listener_select_proto(connec, vec!["/proto1\n".to_string(), "/proto2\n".to_string()]).await;
+        let res = listener_select_proto(connec.clone(), vec!["/secio/1.0.0\n".to_string(), "/proto2\n".to_string()]).await;
         println!("res:{:?}", res);
+        loop {
+            let mut read_buf = vec![0u8; 10];
+            let res = connec.read_exact(&mut read_buf).await;
+
+            println!("res:{:?},receive:{:?}", res, read_buf);
+        }
     })
 }
